073729ba2e7ffbeb05cbebe0aeb90aa198e31fb7
J.A. Iguacel <infoiguacel@gmail.com>
2020-04-21 20:07:46 +0200

    Updates

---
+++ precache-manifest.bc7777f207453796f9fb2f817b0ee48b.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44def9d1e79a19ca7398f87ec56d1a3a",
+    "url": "/iguacel/index.html"
+  },
+  {
+    "revision": "63f9441a55507a12a0fe",
+    "url": "/iguacel/static/css/main.bd25134a.chunk.css"
+  },
+  {
+    "revision": "4f0c9326341507cb0c25",
+    "url": "/iguacel/static/js/0.bbb8897d.chunk.js"
+  },
+  {
+    "revision": "53966c552e83fc945040a449beaf98e0",
+    "url": "/iguacel/static/js/0.bbb8897d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1619ce9ee7786d6370fb",
+    "url": "/iguacel/static/js/1.eedf8560.chunk.js"
+  },
+  {
+    "revision": "cd0c73dd683690ef8a8f",
+    "url": "/iguacel/static/js/10.2fb51c56.chunk.js"
+  },
+  {
+    "revision": "a93848031f35e8313cb4",
+    "url": "/iguacel/static/js/11.843d6d4c.chunk.js"
+  },
+  {
+    "revision": "18d5c7b3afb38bd29df8",
+    "url": "/iguacel/static/js/12.fc0764fb.chunk.js"
+  },
+  {
+    "revision": "a56b07b81243c6625cd3",
+    "url": "/iguacel/static/js/13.83a130f5.chunk.js"
+  },
+  {
+    "revision": "a14f359d54e1f38979c8",
+    "url": "/iguacel/static/js/14.3facade9.chunk.js"
+  },
+  {
+    "revision": "53316ebd32bdcb1159db",
+    "url": "/iguacel/static/js/15.901c5b19.chunk.js"
+  },
+  {
+    "revision": "a17d672b81a738287f78",
+    "url": "/iguacel/static/js/16.81ac7f0a.chunk.js"
+  },
+  {
+    "revision": "6678d5fec219c047abeb",
+    "url": "/iguacel/static/js/17.f9929c65.chunk.js"
+  },
+  {
+    "revision": "85f7f73d1644c90f0c8b",
+    "url": "/iguacel/static/js/18.cb702478.chunk.js"
+  },
+  {
+    "revision": "5f167ee9835879731ab1",
+    "url": "/iguacel/static/js/19.258aedf0.chunk.js"
+  },
+  {
+    "revision": "158ae29203cea71dd743",
+    "url": "/iguacel/static/js/2.86a27adf.chunk.js"
+  },
+  {
+    "revision": "d51c699b4357cb37dcc86d11129f128d",
+    "url": "/iguacel/static/js/2.86a27adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef91a5e2aa346661c07e",
+    "url": "/iguacel/static/js/20.b2abb543.chunk.js"
+  },
+  {
+    "revision": "80f8cd42f62619e44be4",
+    "url": "/iguacel/static/js/21.0ffc1c9e.chunk.js"
+  },
+  {
+    "revision": "440da2bb39f0ea4d7e26",
+    "url": "/iguacel/static/js/22.f32b2e36.chunk.js"
+  },
+  {
+    "revision": "11632a58e510552bf3ae8fe3d05874e6",
+    "url": "/iguacel/static/js/22.f32b2e36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7bbf0f1947f75026bb3",
+    "url": "/iguacel/static/js/5.81ba510d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/iguacel/static/js/5.81ba510d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a37bde990b83db2bf7a0",
+    "url": "/iguacel/static/js/6.f45eba37.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/iguacel/static/js/6.f45eba37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8cc0e727b97c6113cf5",
+    "url": "/iguacel/static/js/7.28c10383.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/iguacel/static/js/7.28c10383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01635bbddbcd0ade658",
+    "url": "/iguacel/static/js/8.b6f39440.chunk.js"
+  },
+  {
+    "revision": "6ccda9a27c5f3f954ecd",
+    "url": "/iguacel/static/js/9.2345e8bd.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/iguacel/static/js/9.2345e8bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f9441a55507a12a0fe",
+    "url": "/iguacel/static/js/main.55b60195.chunk.js"
+  },
+  {
+    "revision": "4350c18f1d4f410b011a",
+    "url": "/iguacel/static/js/runtime-main.00a5de68.js"
+  },
+  {
+    "revision": "b3d490aaafc0687821b93fccb6af7ead",
+    "url": "/iguacel/static/media/burguer.b3d490aa.mp3"
+  },
+  {
+    "revision": "efdadc4fdd6f838a12461f7c37b1a2f2",
+    "url": "/iguacel/static/media/click.efdadc4f.mp3"
+  },
+  {
+    "revision": "b3d490aaafc0687821b93fccb6af7ead",
+    "url": "/iguacel/static/media/info.b3d490aa.mp3"
+  },
+  {
+    "revision": "0a742f75a580915325c41e24a6071691",
+    "url": "/iguacel/static/media/light.0a742f75.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/iguacel/precache-manifest.120031678b256770e1ffed78d6154f8b.js"
+  "/iguacel/precache-manifest.bc7777f207453796f9fb2f817b0ee48b.js"
 );
 
 self.addEventListener('message', (event) => {
